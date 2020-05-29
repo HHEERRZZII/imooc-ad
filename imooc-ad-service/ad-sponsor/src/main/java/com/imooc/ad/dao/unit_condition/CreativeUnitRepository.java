7f0239d307345b4e50eb641f16018f7397e383ec
Herzi <529027282@qq.com>
2020-05-30 00:01:28 +0800

    创建用户功能实现

--- imooc-ad-service/ad-sponsor/src/main/java/com/imooc/ad/dao/unit_condition/CreativeUnitRepository.java
+++ imooc-ad-service/ad-sponsor/src/main/java/com/imooc/ad/dao/unit_condition/CreativeUnitRepository.java
@@ -4,5 +4,5 @@ import com.imooc.ad.entity.unit_condition.CreativeUnit;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CreativeUnitRepository  extends JpaRepository<CreativeUnit, Long> {
-    
+
 }
