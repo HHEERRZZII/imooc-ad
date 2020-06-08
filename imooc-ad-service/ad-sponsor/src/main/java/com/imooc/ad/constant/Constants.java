2330f3c54c4d19ee5b3b23176176c5b031ff9c21
Herzi <529027282@qq.com>
2020-06-08 23:44:32 +0800

    一些文件的说明。

--- imooc-ad-service/ad-sponsor/src/main/java/com/imooc/ad/constant/Constants.java
+++ imooc-ad-service/ad-sponsor/src/main/java/com/imooc/ad/constant/Constants.java
@@ -2,7 +2,9 @@ package com.imooc.ad.constant;
 
 public class Constants {
     //内部类
-    public static class ErrorMsg {
+    public  class ErrorMsg {
+        //说白了就是点点点，因为外部类可以调用内部类的属性，从而可以实现一些说明功能。记住要加static
+        //先实例化再初始化，static可以跳过实例化，直接初始化使用，所以可以减少内存的使用。
         public static final String REQUEST_PARAM_ERROR = "请求参数错误";
         public static final String SAME_NAME_ERROR = "存在同名用户";
     }
